aa494672bb57165e7ddec189d14a25bf56d303f9
Cedsi56 <cedsitails@gmail.com>
2023-10-18 22:29:44 +0200

    touch grass eat ass

--- src/data/lines.ts
+++ src/data/lines.ts
@@ -8,40 +8,46 @@ export type Line = {
 export const lines: Line[] = [
     {
         id : 0,
-        text : "This is my joker moment, and this is my neutral special",
-        audio : "jokermoment.mp3",
-        video : 1000
-    },
-    {
-        id : 1,
         text : "So he goes 'BREAKING NEWS *ACTUAL IRL TRAGEDY*'",
         audio : "breakingnews.mp3",
         video : 982
     },
     {
-        id : 2,
+        id : 1,
         text : "Evelynn's behind",
         audio : "evelynnbehind.mp3",
         video : 1008
     }, {
-        id : 3,
+        id : 2,
         text : "What if we kissed under the toplane outer turret ?",
         audio : "kissturret.mp3",
         video : 983
     }, {
-        id : 4,
+        id : 3,
         text : "Rek'Sai is not a dude okay, she's my queen",
         audio : "queenreksai.mp3",
         video : 1017
     }, {
-        id : 5,
+        id : 4,
         text : "I'm Will, 21, and I never learned how to fucking base properly",
         audio : "will21base.mp3",
         video : 1005
     }, {
-        id : 6,
+        id : 5,
         text : "It is because we fall that the climb must be our destination",
         audio : "climbdestination.mp3",
         video : 988
-    }
+    },
+    {
+        id : 6,
+        text : "This is my joker moment, and this is my neutral special",
+        audio : "jokermoment.mp3",
+        video : 1000
+    },
+    {
+        id : 7,
+        text : "You know I'll try anything once but I gotta say this grass thing really isn't doing it for me",
+        audio : "touchgrass.mp3",
+        video : 1003
+    },
 ]
